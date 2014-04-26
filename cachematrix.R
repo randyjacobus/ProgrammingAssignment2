d8cdc5ebfdf9b05ed8695844011abefcc63cc06d
randyjacobus <rjacobus@asallc.com>
2014-04-26 09:37:09 -0500

    Cache -Solution to Programing Assignment 2
    This is the solution to programming assignment #2 for R Programming. It
    shows how to use the cache to make functions more efficient.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will return the inverse of a matrix. However, they will first check to see if the inverse has already been calculated and if so it will return it from the cache.
 
-## Write a short comment describing this function
+
+## This function will take an inversible numeric matrix and return a list of of four items.
+## The items are are for functions set, get, setinverse, and getinverse which will be used in cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
 
+		m <- NULL				# initialize m
+        	set <- function(y) {		# sets matrix into cache
+                x <<- y			
+                m <<- NULL
+        }
+        get <- function() x		# gets matrix
+        setinverse <- function(solve) m <<- solve 	# sets the inverse in cache
+        getinverse <- function() m		# gets the inverse from cache
+        list(set = set, get = get,		# list of functions
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This is a client function that expects that the special matirix created from makeCacheMatrix and the output is the inverse. This output is only computed if it is not found in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the invers of 'x'
+        m <- x$getinverse()	# query cache
+        if(!is.null(m)) {		# check to see if cache exists
+                message("getting cached data") 
+                return(m)		# return the cache
+        }
+        data <- x$get()		# if no cache
+        m <- solve(data, ...)	# compute inverse
+        x$setinverse(m)		# save in cache
+        m
 }
